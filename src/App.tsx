ddfe6381759b95c3f954ea63a7c5933e4198b366
Ragnorosss <gost45678@gmail.com>
2025-03-20 17:04:50 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/screens/home/Home';
+import Login from './components/screens/Login';
+import Register from './components/screens/Register';
+import RootLayout from './components/layout/RootLayout';
+import AuthLayout from './components/layout/AuthLayout';
+import Orders from './components/screens/Orders';
+import AnimateWrapper from './components/layout/AnimateWrapper';
+import Order from './components/screens/order/Order';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AnimateWrapper>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="orders/:id" element={<Order />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </AnimateWrapper>
+  );
 }
 
-export default App
+export default App;
